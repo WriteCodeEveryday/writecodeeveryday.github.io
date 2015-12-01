5b24937c63e2483b4bbe98a09169e8d077df62cf
Lazaro Herrera <lazherrera@gmail.com>
2015-12-01 02:21:41 -0500

    prepend

--- projects/bittree/gallery.js
+++ projects/bittree/gallery.js
@@ -2,7 +2,7 @@ $( document ).ready(function readyJQuery() {
   var myFirebaseRef = new Firebase("https://bitaddressstorage.firebaseio.com/pictures");
   myFirebaseRef.orderByChild("time").limitToLast(25).on("child_added", function(snapshot) {
     var data = snapshot.val();
-    $("#gallery").append(
+    $("#gallery").prepend(
       $('<div/>', {'class': 'col s12 m6'}).append(
         $('<div/>', {'class': 'card'}).append(
           $('<div/>', {'class': 'card-image'}).append(

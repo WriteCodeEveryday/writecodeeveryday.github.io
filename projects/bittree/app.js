75bc7fb659f9548b8f623df3eb46fe33d1c87efb
arubinofaux <arubinofaux@gmail.com>
2015-12-02 12:27:50 -0500

    node app ready for heroku

---
+++ projects/bittree/app.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public/assets'));
+
+var views_dir = __dirname + '/public/views/';
+
+app.get('/', function (req, res) {
+  res.sendFile(views_dir + 'index.html');
+});
+
+app.get('/gallery', function (req, res) {
+  res.sendFile(views_dir + 'gallery.html');
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})

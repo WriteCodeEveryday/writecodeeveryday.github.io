6c06451efe73606c56997f4644a337288f626010
Lazaro Herrera <lazherrera@gmail.com>
2015-12-14 12:51:28 -0500

    websocket complete

--- projects/bullrunbot/bot.py
+++ projects/bullrunbot/bot.py
@@ -2,10 +2,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
  
-import tweepy, time, sys
+import tweepy, time, sys, websocket
  
 argfile = str(sys.argv[1])
- 
+
 #enter the corresponding information from your Twitter application:
 CONSUMER_KEY = '1234abcd...'#keep the quotes, replace this with your consumer key
 CONSUMER_SECRET = '1234abcd...'#keep the quotes, replace this with your consumer secret key
@@ -14,7 +14,32 @@ ACCESS_SECRET = '1234abcd...'#keep the quotes, replace this with your access tok
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
- 
+
+pricing = {}
+
+def bitfinex_on_open(ws):
+    sub = json.dumps({"event": "subscribe", "channel": "trades", "pair": "BTCUSD"})
+    ws.send(sub)
+    print("Intitalizing Bitfinex Socket")
+
+def bitfinex_on_message(ws, message):
+  sub = json.loads(message) 
+  if isinstance(sub, dict) and sub["event"]:
+    return;
+
+  if sub[1] != "hb" and len(sub) > 2:
+    pricing[time.time()]  = sub[3] 
+
+
+bitfinex = websocket.WebSocketApp("wss://api2.bitfinex.com:3000/ws",
+							on_message = bitfinex_on_message,
+              on_error = on_error,
+              on_close = on_close)
+bitfinex.on_open = bitfinex_on_open
+bitfinex_thread = Thread(target = bitfinex.run_forever)
+
+
+
 filename=open(argfile,'r')
 f=filename.readlines()
 filename.close()

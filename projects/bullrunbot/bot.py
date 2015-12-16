6ebeab046cbe848d91c9693cf49a8889223d87c9
Joshua Echeverri <josheche@gmail.com>
2015-12-16 14:19:06 -0500

    formated currency and tweet

--- projects/bullrunbot/bot.py
+++ projects/bullrunbot/bot.py
@@ -5,38 +5,39 @@
 import json
 import time
 import sys
-import random
-import tweepy # IMPORT
+import random # Generates Random Numbers
+import tweepy # Python Tweeting
+import locale # Currency Library
+import os
 from urllib2 import Request, urlopen, URLError
 
+#enter the corresponding information from your Twitter application into Heroku:
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
-# removed command line arguments bc theres none
-
-#enter the corresponding information from your Twitter application:
-CONSUMER_KEY = '1234abcd...'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = '1234abcd...'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '1234abcd...'#keep the quotes, replace this with your access token
-ACCESS_SECRET = '1234abcd...'#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 pricing = {}
+locale.setlocale( locale.LC_ALL, '' )
 
 if __name__ == "__main__":
     def check_bullrun():
         diff = pricing["second"] - pricing["first"]
-        if (diff > 20):
-            api.update_status("Bitcoin is a bull's best friend, especially since it's climbed $" + str(diff) + " over the last hour",)
-
+        api.update_status("Bitcoin price difference over the last hour: " + locale.currency(diff) + ".")
+        if (diff > (pricing["second"]/20)):
+            api.update_status("#BITCOINBULLRUN ALERT: The price of Bitcoin is up " + locale.currency(diff) + " (" + str("{:2.2f}".format((diff/pricing["second"]) * 100 )) + "%) over the last hour.")
     def get_coinbase():
-        request = Request('https://api.coinbase.com/v2/prices/buy')
+        request = Request('https://api.coinbase.com/v2/prices/spot')
         response = urlopen(request)
         sub = json.loads(response.read())
         return float(sub["data"]["amount"])
 
     while True:
         pricing["first"] = get_coinbase()
-        time.sleep(60 * 60)#Tweet every 60 minutes (5 for testing)
+        time.sleep(60 * 60) #Tweet every 60 minutes (5 for testing)
         pricing["second"] = get_coinbase()
         check_bullrun()
